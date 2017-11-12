3f183c91586a0380faa72b0f43a5f3b765d1accf
batuhansahan <batuhansahan@trakya.edu.tr>
2017-11-12 15:59:47 +0200

    Added header album title, name, thumbnail image and added album image, scrollable view

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { ScrollView } from 'react-native';
 import axios from 'axios';
 import AlbumDetail from './AlbumDetail';
 
@@ -7,7 +7,6 @@ class AlbumList extends Component {
     state = {
         albums: []
     };
-
 
     componentWillMount() {
         axios.get('https://rallycoding.herokuapp.com/api/music_albums')
@@ -22,9 +21,9 @@ class AlbumList extends Component {
 
     render() {
         return (
-            <View>
+            <ScrollView>
                 {this.renderAlbums()}
-            </View>
+            </ScrollView>
         );
     }
 }
